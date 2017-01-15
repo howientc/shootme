6729a758037379f209c9434e464ab46c03d502f9
howientc <howie@nextthing.co>
2017-01-14 17:55:15 -0800

    wip

--- src/slideshow.py
+++ src/slideshow.py
@@ -12,15 +12,6 @@ from itertools import cycle
 import Tkinter as tk
 class App(tk.Tk):
     
-    
-    root = Tk()
-
-# make it cover the entire screen
-    w, h = root.winfo_screenwidth(), root.winfo_screenheight()
-    root.overrideredirect(1)
-    root.geometry("%dx%d+0+0" % (w, h))
-
-
 
     '''Tk window/label adjusts to size of image'''
     def __init__(self,  x, y, delay):
