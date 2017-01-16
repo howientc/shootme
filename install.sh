404cea58d6e10e02091aba8167ba618296d5bd64
howientc <howie@nextthing.co>
2017-01-16 21:17:32 +0000

    fixed script

--- install.sh
+++ install.sh
@@ -1,4 +1,12 @@
-sudo apt-get install python-pip
-sudo apt-get install python python-tk idle python-pmw python-imaging
-sudo pip install playsound
-sudo apt-get install python-pygame
+sudo apt-get install git build-essential python-pip python-dev python-pygame flex bison -y
+sudo pip install flask
+git clone https://github.com/atenart/dtc
+cd dtc
+make
+sudo  make install PREFIX=/usr
+cd ..
+git clone git://github.com/xtacocorex/CHIP_IO.git
+cd CHIP_IO
+sudo python setup.py install
+cd ..
+sudo rm -rf CHIP_IO
